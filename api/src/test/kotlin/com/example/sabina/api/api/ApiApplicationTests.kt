44819a9df68c3bf3405d5793ff6a540007941ea5
Fuad Muhic <muhic.fuad.git@gmail.com>
2022-12-26 01:01:23 +0100

    Add api and web

---
+++ api/src/test/kotlin/com/example/sabina/api/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sabina.api.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ web/src/components/home/Home.js
@@ -0,0 +1,13 @@
+import './home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home-header">
+                hello banking web
+            </div>
+        </div>
+    );
+}
+
+export default Home;
