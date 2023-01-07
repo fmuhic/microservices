cad4ec76053ced1c79bd20ffab962fba68ad2553
Fuad Muhic <muhic.fuad.git@gmail.com>
2023-01-07 02:45:41 +0100

    add prometheus to api

--- api/src/main/kotlin/com/example/sabina/api/controllers/UserController.kt
+++ api/src/main/kotlin/com/example/sabina/api/controllers/UserController.kt
@@ -2,16 +2,25 @@ package com.example.sabina.api.controllers
 
 import com.example.sabina.api.models.User
 import com.example.sabina.api.repositories.UserRepository
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/user")
-class UserController(val userRepository: UserRepository) {
+class UserController(
+    val userRepository: UserRepository,
+    metricsRegistry: MeterRegistry
+) {
+
+    private val listUsersCounter: Counter = metricsRegistry.counter("list_users_total")
 
     @GetMapping
-    fun listUsers(): ResponseEntity<Iterable<User>> = ResponseEntity(userRepository.findAll(), HttpStatus.OK)
+    fun listUsers(): ResponseEntity<Iterable<User>> = ResponseEntity(userRepository.findAll(), HttpStatus.OK).also {
+        listUsersCounter.increment()
+    }
 
     @PostMapping
     fun createUser(@RequestBody newUser: User): ResponseEntity<Any> {
