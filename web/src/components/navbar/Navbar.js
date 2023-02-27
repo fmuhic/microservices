4858a8445f691ccb6564590726be6d06d643dbf0
Sabina <sabina.muhic@abc-techgroup.com>
2023-02-27 16:47:48 +0100

    Sidebar; navbar dorade

--- web/src/components/navbar/Navbar.js
+++ web/src/components/navbar/Navbar.js
@@ -1,14 +1,13 @@
 import React from "react";
 import './navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+import { light, regular, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function Navbar() {
     return (
         <div className="my-navbar">
-            <div className="navbar-title">
-                <FontAwesomeIcon icon={solid('building-columns')} /> Banka
-            </div>
+            <div className="navbar-title"><FontAwesomeIcon icon={solid('building-columns')} /> Banka</div>
+            <div id="navbar-bars"><FontAwesomeIcon icon={solid('bars')} /></div>
         </div>
     );
 }
--- web/src/index.js
+++ web/src/index.js
@@ -7,11 +7,13 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import Home from './components/home/Home';
 import Navbar from './components/navbar/Navbar';
 import './index.css';
+import Sidebar from './components/sidebar/Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Navbar />
         <Home />
+        <Sidebar />
     </React.StrictMode>
 );
