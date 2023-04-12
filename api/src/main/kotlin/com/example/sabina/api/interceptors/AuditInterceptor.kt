e9cfada1d77ba105cc2ad0039ae38dfdf48dd5ec
Fuad Muhic <fuad.muhic@infobip.com>
2023-04-12 17:23:30 +0200

    interceptor poc

--- api/src/main/kotlin/com/example/sabina/api/interceptors/AuditInterceptor.kt
+++ api/src/main/kotlin/com/example/sabina/api/interceptors/AuditInterceptor.kt
@@ -1,16 +1,20 @@
 package com.example.sabina.api.interceptors
 
+import com.example.sabina.api.models.User
+import com.google.gson.Gson
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import mu.KotlinLogging
+import mu.KotlinLogging.logger
 import org.springframework.lang.Nullable
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.ModelAndView
-import java.lang.Exception
+import java.util.stream.Collectors
+
 
 class AuditInterceptor: HandlerInterceptor {
     companion object {
-        private val log = KotlinLogging.logger {}
+        private val log = logger {}
+        private val gson = Gson()
     }
 
     override fun preHandle(
@@ -18,7 +22,11 @@ class AuditInterceptor: HandlerInterceptor {
         response: HttpServletResponse,
         handler: Any
     ): Boolean {
-        log.info { "Pre handle: $request" }
+        request.headerNames.asIterator().forEach {
+            log.info { "Pre handle header: $it : ${request.getHeader(it)}" }
+        }
+        val user = gson.fromJson(request.reader.lines().collect(Collectors.joining()), User::class.java)
+        println("Body bytes: $user")
         return true
     }
 
