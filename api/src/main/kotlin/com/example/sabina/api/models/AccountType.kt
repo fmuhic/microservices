e1e35a47267646edf87a1974de08ee663a76896a
Fuad Muhic <fuad.muhic@infobip.com>
2023-09-25 18:19:33 +0200

    add basic layout to web

--- api/src/main/kotlin/com/example/sabina/api/models/AccountType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sabina.api.models
-
-enum class TransactionType {
-    REGULAR,
-    DEPOSIT,
-    WITHDRAW
-}
--- web/src/components/layouts/DefaultLayout.jsx
+++ web/src/components/layouts/DefaultLayout.jsx
@@ -1,18 +1,41 @@
+import "./defaultLayout.css"
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBitcoin } from '@fortawesome/free-brands-svg-icons'
+import {
+    faRightFromBracket,
+    faUser,
+    faHouse,
+    faWallet
+} from '@fortawesome/free-solid-svg-icons'
+
 
 export default function DefaultLayout() {
     return (
         <div className="default-layout">
-            <header>
-                <nav>
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/transactions'>Transactions</NavLink>
-                </nav>
-            </header>
-            <main>
-                <Outlet />
-            </main>
+            <nav className='header-navbar'>
+                <div className='header-navbar-content'>
+                    <div className='header-navbar-left'>
+                        <FontAwesomeIcon className='logo-icon' icon={faBitcoin} />
+                    </div>
+                    <div className='header-navbar-right'>
+                        <FontAwesomeIcon className='nav-icon' icon={faRightFromBracket} />
+                        <FontAwesomeIcon className='nav-icon' icon={faUser} />
+                    </div>
+                </div>
+            </nav>
+            <nav className='page-navbar'>
+                <NavLink className='nav-link' to='/'>
+                    <FontAwesomeIcon className='nav-link-icon' icon={faHouse} />
+                    <div>Home</div>
+                </NavLink>
+                <NavLink className='nav-link' to='/transactions'>
+                    <FontAwesomeIcon className='nav-link-icon' icon={faWallet} />
+                    <div>Transactions</div>
+                </NavLink>
+            </nav>
+            <Outlet />
         </div>
     )
 }
